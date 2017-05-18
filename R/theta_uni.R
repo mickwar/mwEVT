5c9975665b26586e9505a67e31fb3859fed18340
mickey <mickey.a.warner@gmail.com>
2017-05-18 11:17:39 -0700

    yes

--- R/theta_uni.R
+++ R/theta_uni.R
@@ -1,16 +1,18 @@
-#' Extremal index estimation based on Ferro and Segers (2003)
+#' Extremal index estimation
 #'
 #' @description
 #' Uses the likelihood provided in Ferro and Segers (2003) in a Bayesian setting
 #' to obtain posterior samples of the extremal index. Separates exceedances into
 #' clusters.
 #'
-#' @param y         Numeric, sequence of depedent random variables. May be a matrix.
-#'                  See details.
-#' @param u         Numeric, the threshold. Defaults to quantile(y, 0.90).
-#' @param ord       Numeric, vector of length R = NCOL(y). Defaults to 1:R. See details.
-#' @param prior     List.
-#' @param ...       Additional arguments passed to mwBASE::mcmc_sampler.
+#' @param y             Numeric, sequence of depedent random variables. May be a matrix.
+#'                      See details.
+#' @param u             Numeric, the threshold. Defaults to quantile(y, 0.90).
+#' @param ord           Numeric, vector of length R = NCOL(y). Defaults to 1:R. See details.
+#' @param prior         List.
+#' @param likelihood    Character, either "ferro" or "suveges" (default).
+#' @param method        Character, either "classical" or "bayesian" (default).
+#' @param ...           Additional arguments passed to mwBASE::mcmc_sampler.
 #'
 #' @details
 #' The data y is expected to be observed values along an evenly-spaced grid of width 1.
@@ -18,44 +20,14 @@
 #' the case when working with multiple realizations from the same process or with
 #' specific seasons on a time-series.
 #'
+#' The 'classical' option for method produces the estimates given by either Ferro and
+#' Segers (2003) or Suveges (2007). When method == 'bayesian' the likelihoods given in
+#' the aforementioned papers are used to produce posterior samples for theta.
+#'
 #' @export
-#' @examples
-#' ### Generate dependent random variables with standard Frechet marginals
-#' set.seed(1)
-#' n = 365*50     # 50 years of data, no leap years
-#'
-#' theta = 0.25
-#' W = -1/log(runif(n))
-#' y = double(n)
-#' y[1] = W[1] / theta
-#' for (i in 2:n)
-#'     y[i] = max((1-theta)*y[i-1], W[i])
-#'
-#' out = theta_uni(y, quantile(y, 0.95), nburn = 40000, nmcmc = 20000)
-#'
-#' mean(out$mcmc)     # 0.277
-#' quantile(out$mcmc, c(0.025, 0.975))    # c(0.248, 0.306)
-#'
-#'
-#' ### Generate dependent random variables with standard Frechet marginals,
-#' ### but only for winter, say.
-#' set.seed(1)
-#' R = 50   # number of seasons (years)
-#' n = 90   # number of observations (days) per season
-#'
-#' theta = 0.25
-#' W = matrix(-1/log(runif(n*R)), n, R)
-#' y = matrix(0, n, R)
-#' y[1,] = W[1,] / theta
-#' for (i in 2:n)
-#'     y[i,] = apply(rbind((1-theta)*y[i-1,], W[i,]), 2, max)
-#'
-#' out = theta_uni(y, quantile(y, 0.95), nburn = 40000, nmcmc = 20000)
-#'
-#' mean(out$mcmc)     # 0.267
-#' quantile(out$mcmc, c(0.025, 0.975))    # c(0.214, 0.325)
+#' @example examples/ex_theta_uni.R
 
-theta_uni = function(y, u, ord, prior, ...){
+theta_uni = function(y, u, ord, prior, likelihood, method, ...){
     require(mwBASE)
 
     n = NROW(y)
@@ -74,6 +46,18 @@ theta_uni = function(y, u, ord, prior, ...){
     if (missing(u))
         u = quantile(y, 0.90)
 
+    if (missing(likelihood))
+        likelihood = "suveges"
+
+    if (!(likelihood %in% c("ferro", "suveges")))
+        stop("likelihood must be either 'ferro' or 'suveges'.")
+
+    if (missing(method))
+        method = "classical"
+
+    if (!(method %in% c("classical", "bayesian")))
+        stop("method must be either 'classical' or 'bayesian'.")
+
     
     exceed = which(y > u)
     Tu = diff(exceed)
@@ -86,34 +70,78 @@ theta_uni = function(y, u, ord, prior, ...){
 #   if (R > 1)
 #       m1 = m1 + (sum(which(x > u) %% n == 0) + sum(which(x > u) %% n == 1)) / 2
 
-    if (missing(prior))
-        prior = list("theta_a" = 1/2, "theta_b" = 1,
-            "p_a" = emp.p*100, "p_b" = (1-emp.p)*100)
+    if (method == "classical"){
+        if (likelihood == "ferro"){
+            intervals_est = function(Tu){
+                if (length(Tu) == 0)
+                    return (1)
+                if (max(Tu) <= 2){
+                    out = min(1, 2*(sum(Tu))^2 / ( length(Tu) * sum(Tu^2) ) )
+                } else {
+                    out = min(1, 2*(sum(Tu - 1))^2 / (length(Tu)*sum((Tu-1)*(Tu-2))))
+                    }
+                return (out)
+                }
+            theta_hat = intervals_est(Tu)
+        } else {
+            suveges_mle = function(Tu){
+                ((1-emp.p)*sum(Tu - 1)+2*(N-1)-m1 -
+                    sqrt(((1-emp.p)*sum(Tu - 1)+2*(N-1)-m1)^2 - 8*(N-1-m1)*(1-emp.p)*sum(Tu-1))) / 
+                    (2*(1-emp.p)*sum(Tu-1))
+                }
+            theta_hat = suveges_mle(Tu)
+            }
+    } else {
+        if (missing(prior)){
+            if (likelihood == "ferro")
+                prior = list("theta_a" = 1/2, "theta_b" = 1,
+                    "p_a" = emp.p*100, "p_b" = (1-emp.p)*100)
+            if (likelihood == "suveges")
+                prior = list("theta_a" = 1/2, "theta_b" = 1)
+            }
 
-    calc.post = function(dat, param){
-        theta = param[1]
-        p = param[2]
-        if (theta <= 0 || theta > 1)
-            return (-Inf)
-        if (p <= 0 || p >= 1)
-            return (-Inf)
+        if (likelihood == "ferro"){
+            calc.post = function(dat, param){
+                theta = param[1]
+                p = param[2]
+                if (theta <= 0 || theta > 1)
+                    return (-Inf)
+                if (p <= 0 || p >= 1)
+                    return (-Inf)
 
-        # Likelihood (Ferro, Eq. 3)
-        out = m1 * log(1 - theta*p^theta) + (N - 1 - m1)*(log(theta) + log(1-p^theta)) +
-            theta*log(p)*sum(dat - 1)
+                # Likelihood (Ferro and Segers, Eq. 3)
+                out = m1 * log(1 - theta*p^theta) + (N - 1 - m1)*(log(theta) + log(1-p^theta)) +
+                    theta*log(p)*sum(dat - 1)
 
-        # Priors
-        out = out + dbeta(theta, prior$theta_a, prior$theta_b, log = TRUE)
-        out = out + dbeta(p, prior$p_a, prior$p_b, log = TRUE)
-        return (out)
+                # Priors
+                out = out + dbeta(theta, prior$theta_a, prior$theta_b, log = TRUE)
+                out = out + dbeta(p, prior$p_a, prior$p_b, log = TRUE)
+                return (out)
+                }
+        } else {
+            calc.post = function(dat, param){
+                theta = param[1]
+                if (theta <= 0 || theta > 1)
+                    return (-Inf)
+
+                # Likelihood (Suveges, Eq. 1)
+                out = m1 * log(1 - theta) + 2*(N - 1 - m1)*log(theta) - theta*(1-emp.p)*sum(dat - 1)
+
+                # Priors
+                out = out + dbeta(theta, prior$theta_a, prior$theta_b, log = TRUE)
+                return (out)
+                }
+            }
+        mcmc_out = mcmc_sampler(data = Tu, target = calc.post,
+            nparam = ifelse(likelihood == "ferro", 2, 1), ...)
+        mcmc_out$param = as.matrix(mcmc_out$param)
+
+        theta_hat = mean(mcmc_out$param[,1])
         }
-    mcmc_out = mcmc_sampler(data = Tu, target = calc.post, nparam = 2, ...)
-
-    theta.hat = mean(mcmc_out$param[,1])
 
 
     ### Decluster
-    C = floor(theta.hat*N)+1
+    C = floor(theta_hat*N)+1
     C = min(C, N-1)
     tmp = sort(Tu, decreasing = TRUE)
     T_C = tmp[C]
@@ -146,5 +174,9 @@ theta_uni = function(y, u, ord, prior, ...){
     # Get the greatest value within each (independent) cluster
     ind.obs = sapply(nice.C, max)
 
-    return (list("y" = ind.obs, "N" = N, "T_C" = T_C, "mcmc" = mcmc_out$param[,1]))
+    if (method == "classical"){
+        return (list("y" = ind.obs, "N" = N, "T_C" = T_C, "theta" = theta_hat))
+    } else {
+        return (list("y" = ind.obs, "N" = N, "T_C" = T_C, "mcmc" = mcmc_out$param[,1]))
+        }
     }
